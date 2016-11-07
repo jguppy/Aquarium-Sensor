2d53aa069747de024c96c2c5397cdac090214988
jguppy <jarrod.guppy@my.jcu.edu.au>
2016-11-07 16:47:19 +1000

    Update TemperatureLogBootScript.py

--- TemperatureLogBootScript.py
+++ TemperatureLogBootScript.py
@@ -4,9 +4,9 @@ import time
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-temp_sensor1 = '/sys/bus/w1/devices/28-0000005e2fdc3/w1_slave'
-temp_sensor2 = '/sys/bus/w1/devices/28-0000005e2fdc3/w1_slave'
-temp_sensor3 = '/sys/bus/w1/devices/28-0000005e2fdc3/w1_slave'
+Room_temp_sensor = '/sys/bus/w1/devices/28-0000005e2fdc3/w1_slave'
+Display_temp_sensor2 = '/sys/bus/w1/devices/28-0000005e2fdc3/w1_slave'
+Sump_temp_sensor3 = '/sys/bus/w1/devices/28-0000005e2fdc3/w1_slave'
 
 def temp_raw():
 	f = open(temp_sensor, 'r')
