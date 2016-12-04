228f33153e4a462f67ffe36b40384860ebca271b
jguppy <jarrod.guppy@my.jcu.edu.au>
2016-12-04 19:02:06 +1000

    Update create.py

--- create.py
+++ create.py
@@ -1,42 +1,63 @@
 import csv
 import sys
-import datetime
-import json
+
+## ask for file details
+print("Creating new file")
+name = input("Enter the name of the file:")
+extension = input("Enter the extension type:")
+file_name= name+"." +extension
+
+	## need to figure out how to call variables between functions
+	## return function doesnt seem to be working for some reason
 
 def create():
-	print("Creating new file")
-	name = input('Enter the name of the file:')
-	extension = input("Enter the extension type:")
+	## creates the file intially
+	## and also stores the file name for later use
 
-	try:
-		file_name=name+"."+extension
-		return file_name
-		new_file=open(file_name,'a')
+	new_file=open(file_name,'a')
 
-		print("New file created")
-		
-		#return file_name
-		new_file.close()
+	print("New file created")
 
-	except ValueError:
-		print("Error occured")
+	new_file.close()
 
 def writer_module():
-	file_name = create()
-	
+	## this calls the create module
+	## and then asks for new data to be entered
+	## it is then added to the bottum of the created/existing tsv file
+
 	new_data = input('Enter you data seperated by tabs:')
 	new = str(new_data)
 	print("Writing data to file")
 
 	with open(file_name,'a') as updated_file:
 		updated_file.write(new+'\n')
-		#read_data = updated_file.read()
-	sys.exit(0)
 
+def adding_more():
+	new_data = input('Enter you data seperated by tabs:')
+	new= str(new_data)
+	print("Adding extra data to file")
 
-writer_module()
+	with open(file_name,'a') as second_file:
+		second_file.write(new+'\n')
 
-#create()
-#writer_module(create)
+def do_you_want_more_data():
+
+	answer = input("Do you want to add more data? [Y/N]")
+	if answer=="Y":
+		print("Ok lets add some more then...")
+		adding_more_data = adding_more()
+		more_data_again = more_data()
+	elif answer=="N":
+		print("Ok. bye then")
+		sys.exit(0)
+	else:
+		print("Please answer Y or N")
+
+def main():
+	create()
+	writer_module()
+	do_you_want_more_data()
+
+main()
 
 
